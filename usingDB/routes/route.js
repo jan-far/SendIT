5eb15d34e1e766ab20f252773e951150e7f43f33
jan-far <ligalifarouk@gmail.com>
2020-07-22 20:12:16 +0100

    Update route.js

--- usingDB/routes/route.js
+++ usingDB/routes/route.js
@@ -1,6 +1,6 @@
 const router = require("express").Router(config.accessLevels.user)
 import Parcel from "../controllers/parcels";
-import Users from '../controllers/users';
+import Right from '../controllers/users';
 import auth from '../middleware/auth';
 import config from '../config/config';
 
@@ -11,8 +11,9 @@ router.get('/parcels/:id', auth.userRole, Parcel.getOneParcel);
 router.put('/parcels/:id', auth.userRole, Parcel.updateParcel);
 router.delete('/parcels/:id/cancel',auth.userRole, Parcel.cancelParcel);
 
+router.post('/auth/signup', Right.User.create);
+router.post('/auth/signin', Right.User.login);
 
-router.post('/auth/signup', Users.create);
-router.post('/auth/signin', Users.login);
+router.post('/admin/signup', Right.Admin.create);
 
 export default router;
